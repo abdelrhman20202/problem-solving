00c44c51dd12bc6fc61e6bfdfa4292947855a42e
abdelrhman_rabie3 <91215468+abdelrhman20202@users.noreply.github.com>
2022-12-12 18:24:06 +0200

    Add files via upload

---
+++ problem3.c
@@ -0,0 +1,16 @@
+int maxProduct(int* nums, int numsSize)
+{
+int i;
+int max1=0,max2=0;
+for(i=0;i<numsSize;i++)
+{
+if(nums[i]>max1)
+{
+max2=max1;
+max1=nums[i];
+}
+else if(nums[i]>max2 && nums[i]<=max1)
+max2=nums[i];
+}
+return (max1-1)*(max2-1);
+}
